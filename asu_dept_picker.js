6c42fae0c940266a4a54ee2f9a84d311516b976c
Sebastian Nievas <sebinievas@gmail.com>
2016-06-06 13:32:48 -0700

    Fix multiple component render issue

--- asu_dept_picker.js
+++ asu_dept_picker.js
@@ -4,7 +4,6 @@
 (function ($, AsuDeptPicker) {
   Drupal.behaviors.asu_dept_picker = {
     attach: function(context, settings) {
-
       // setup any asu-dept-picker fields
       $('.asu-dept-picker:not([data-reactid])', context).each(function() {
         var delta = $(this).attr('data-delta');
@@ -14,6 +13,8 @@
         config.onChange = function(data) {
           input.val(JSON.stringify(data));
         };
+
+        config.key = delta;
 
         var asu_dept_picker = React.createElement(AsuDeptPicker, config);
         ReactDOM.render(asu_dept_picker, this);
